2e2715a0203c21b1299a7e1bf1a8d16b824d01ff
0218hy <leehayoung0218@gmail.com>
2026-05-20 18:03:17 +0800

    Added drop down picker

--- src/app/(user)/visualizer.tsx
+++ src/app/(user)/visualizer.tsx
@@ -1,12 +1,15 @@
 import Button from '@/src/components/Button';
 import { Text } from '@/src/components/Themed';
 import Colors from '@/src/constants/Colors';
+import { algorithmRegistry } from '@/src/features/visualizer/algorithmRegistry';
+import { bubbleSort } from '@/src/features/visualizer/algorithms/bubbleSort';
 import { Controls } from '@/src/features/visualizer/components/Controls';
 import { SortingVisualizer } from '@/src/features/visualizer/components/SortingVisualizer';
 import { useVisualizer } from '@/src/features/visualizer/hooks/useVisualizer';
 import { useRouter } from "expo-router";
 import React, { useState } from 'react';
 import { Alert, ScrollView, StyleSheet, TextInput, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 
 export default function Visualizer() {
@@ -15,9 +18,14 @@ export default function Visualizer() {
     const [inputText, setInputText] = useState("5 3 8 4 1 2");
     const [currentInput, setCurrentInput] = useState([5, 3, 8, 4, 1, 2]);
 
-    // hard coded as of now
+    const [open, setOpen] = useState(false);
+    const [algo, setAlgo] = useState("Bubble Sort");
+    const [items, setItems] = useState(
+        algorithmRegistry.map((algo) => ({ value: algo.name, label: algo.name }))
+    );
+    
     const visualizer = useVisualizer({
-        algorithmKey: "bubbleSort",
+        algorithmName: algo,
         input: currentInput,
     })
 
@@ -44,8 +52,16 @@ export default function Visualizer() {
 
             {/* Visualization */}
             <View style={styles.card}>
-                <Text style={styles.title}> Selected Algorithm </Text>
-                <Text> Some drop down option </Text>
+                <Text style={styles.title}> Select Algorithm </Text>
+                <DropDownPicker
+                    open={open}
+                    value={algo}
+                    items={items}
+                    setOpen={setOpen}
+                    setValue={setAlgo}
+                    setItems={setItems}
+                    listMode='SCROLLVIEW'
+                />
 
                 {/* Skia */}
                 <View style={styles.greyCard}>
